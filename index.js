c522333367e04abee5dce431f2b874a2f7f3e4f0
B-i-k-t <64721331+B-i-k-t@users.noreply.github.com>
2021-06-30 09:48:44 +0300

    Update index.js
    update buttons buy and del (worked)

--- index.js
+++ index.js
@@ -20,22 +20,20 @@ function add() {
 }
 function button_save() {
     let save__item = document.createElement('div');
-    save__item.className = "item__save"
-    save__item.setAttribute('id', Date.now()+'id');
+    save__item.className = "item__save";
     let save__name = document.getElementsByClassName('item__name');
     let save__count = document.getElementsByClassName('item__count');
     let save__comment = document.getElementsByClassName('item__comment');
     save__item.innerHTML = "<b>" + save__name[0].value + "</b> " + save__count[0].value + " шт. (" + save__comment[0].value + ")";
     let buy = document.createElement('button');
     buy.className = "btn__buy";
-    buy.id = Date.now();
     buy.innerHTML = "Куплено";
-    buy.setAttribute('onclick',"button_buy()");
+    buy.setAttribute('onclick',"button_buy(this)");
     save__item.append(buy);
     let del = document.createElement('button');
     del.className = "btn__del";
     del.innerHTML = "Удалить";
-    del.setAttribute('onclick',"button_del()");
+    del.setAttribute('onclick',"button_del(this)");
     save__item.append(del);
     document.body.append(save__item);
     let item = document.getElementsByClassName('item__add');
@@ -43,7 +41,15 @@ function button_save() {
       item[0].remove();
     } 
   }
+function button_del(btn) {
+  var item__del = btn.parentNode;
+  item__del.parentNode.removeChild(item__del);
+}
 
+function button_buy(btn) {
+  var item__buy = btn.parentNode;
+  item__buy.style.textDecoration = "line-through";
+}
 function clearAll() {
   let item = document.getElementsByClassName('item__add');
   while(item.length) {
